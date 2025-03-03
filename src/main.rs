508fc8301ceaf31adee0545ab6b33a571bb48008
clamydo <13063485+clamydo@users.noreply.github.com>
2025-03-03 20:11:07 +0100

    using async

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,20 @@
-use hickory_resolver::Resolver;
+use hickory_resolver::{
+    config::{ResolverConfig, ResolverOpts},
+    TokioAsyncResolver,
+};
 
-fn main() {
-    let resolver = Resolver::from_system_conf().unwrap();
-    let response = resolver.tlsa_lookup("_25._tcp.mx01.posteo.de.");
+#[tokio::main]
+async fn main() {
+    // Create default ResolverConfig
+    let config = ResolverConfig::default();
 
-    println!("Response: {:?}", response);
+    // Configure options to force UDP
+    let mut opts = ResolverOpts::default();
+    opts.try_tcp_on_error = false;
+
+    // Create resolver with custom config and options
+    let resolver = TokioAsyncResolver::tokio(config, opts);
+    let response = resolver.tlsa_lookup("_25._tcp.mx01.posteo.de.").await;
+
+    println!("DNS Response: {:?}", response);
 }
